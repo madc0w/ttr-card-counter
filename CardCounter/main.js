796393bfe29f625660df32a22c30c3bf3e6cb8f0
mad <marc@balloonary.io>
2020-10-23 10:14:33 +0200

    .

--- CardCounter/main.js
+++ CardCounter/main.js
@@ -1,12 +1,20 @@
 const maxDepth = 4;
 const colors = ['r', 'b', 'g', 'y', 'o', 'v', 'k', 'w', '*'];
 let deck = [];
+// let deck = ["r", "g", "r", "g", "r", "g", "r", "g", "r", "g", "r", "g", "r", "y", "r", "y", "r", "y", "y", "r", "y", "r", "y", "r", "o", "*", "k", "*", "k", "*", "k", "*", "k", "*", "k", "*", "k", "*", "k", "o", "w", "o", "w", "o", "w", "o", "w", "o", "w", "o", "*", "v", "*", "v", "*", "v", "*", "v", "*", "o", "*", "o", "o", "b", "o", "b", "v", "b", "v", "b", "v", "b", "v", "b", "k", "b", "k", "b", "w", "b", "w", "b", "w", "b", "w", "b", "g", "y", "g", "y", "g", "y", "g", "y", "g", "y", "g", "y", "o", "v", "k", "v", "k", "v", "k", "v", "w", "*", "w", "w"];
+// let deck = ["k", "y", "*", "b", "v", "o", "y", "g", "b", "y", "r", "w", "v", "*", "k", "*", "o", "y", "v", "r", "r", "k", "y", "k", "g", "b", "v", "b", "g", "o", "o", "y", "w", "g", "r", "y", "y", "g", "o", "*", "o", "w", "o", "v", "w", "*", "o", "y", "g", "g", "w", "o", "*", "g", "w", "k", "g", "*", "y", "g", "k", "k", "o", "v", "b", "r", "b", "w", "v", "o", "w", "r", "w", "r", "v", "v", "g", "k", "*", "y", "w", "k", "r", "b", "v", "*", "*", "g", "b", "r", "r", "b", "r", "k", "w", "*", "k", "v", "v", "w", "b", "*", "r", "y", "*", "*", "b", "k", "o", "b"];
+
+let nodeId = 0;
 
 function onLoad() {
+	// refreshDeck();
+	// count(true);
+	// count(false);
 	countShuffled();
 }
 
 function enterDeck() {
+	document.getElementById('results').classList.add('hidden');
 	deck = [];
 	refreshDeck();
 
@@ -49,8 +57,9 @@ function countUnshuffled() {
 }
 
 function count(isBlind) {
+	document.getElementById('results').classList.remove('hidden');
 	let sum = 0;
-	const n = 40;
+	const n = 800;
 	for (let i = 0; i < n; i++) {
 		sum += guess(isBlind);
 	}
@@ -71,15 +80,18 @@ function guess(isBlind) {
 	for (const card of deck) {
 		let deepestNode = null;
 		if (!isBlind) {
+			// if (history.length > 4) {
+			// 	console.log();
+			// }
 			for (let depth = Math.min(history.length, maxDepth - 1); depth > 0 && !deepestNode; depth--) {
 				// console.log('depth ', depth);
-				let currNode = root;
-				for (let i = 0; i < depth && currNode; i++) {
-					deepestNode = currNode;
-					currNode = currNode.children.find(e => e.color == history[history.length - 1 - i]);
+				deepestNode = root;
+				for (let i = depth; i > 0 && deepestNode; i--) {
+					deepestNode = deepestNode.children.find(e => e.color == history[history.length - i]);
 				}
 			}
 			// console.log('history', history);
+			// console.log('root', JSON.parse(JSON.stringify(root)));
 			// console.log('deepestNode', deepestNode);
 		}
 
@@ -116,27 +128,36 @@ function guess(isBlind) {
 			currGuess = remaining[Math.floor(Math.random() * remaining.length)];
 		}
 
+		// console.log('currGuess', currGuess);
 		if (card == currGuess) {
 			numCorrect++;
 		}
 		history.push(card);
 		remaining.splice(remaining.indexOf(card), 1);
 
-		let currNode = root;
-		for (let i = Math.min(maxDepth, history.length); i > 0; i--) {
-			const nextNode = currNode.children.find(e => e.color == history[history.length - i]);
-			if (nextNode) {
-				currNode = nextNode;
-			} else {
-				const newNode = {
-					color: history[history.length - i],
-					children: [],
-					count: 0,
-				};
-				currNode.children.push(newNode);
-				currNode = newNode;
+		// add to tree
+		{
+			let currNode = root;
+			let depth = 0;
+			for (let i = Math.min(maxDepth, history.length); i > 0; i--) {
+				const nextNode = currNode.children.find(e => e.color == history[history.length - i]);
+				if (nextNode) {
+					currNode = nextNode;
+				} else {
+					const newNode = {
+						color: history[history.length - i],
+						children: [],
+						count: 0,
+						depth,
+						parent: currNode.color,
+						id: nodeId++,
+					};
+					currNode.children.push(newNode);
+					currNode = newNode;
+				}
+				currNode.count++;
+				depth++;
 			}
-			currNode.count++;
 		}
 		// console.log('card', card);
 		// console.log('root', root);
